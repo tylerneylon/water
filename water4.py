9648b0e9f777b43791f7933ddff4b09fa429fa43
Tyler Neylon <tylerneylon@gmail.com>
2013-04-07 04:21:37 -0700

    water4: Edit goals; write top-level function, v1.
    Next, I might work on get_base_rules.

--- water4.py
+++ water4.py
@@ -9,7 +9,7 @@
 #       
 # In particular, take two steps in writing this file:
 # 1. Get read_lang_def to parse and understand language definition2.water.
-# 2. Use the output of 1 to parse sample1.water.
+# 2. Use the output of 1 to parse the statements at the bottom of that file.
 #
 # Future steps:
 # 3. Actually execute sample1.water.
@@ -17,7 +17,14 @@
 #    longer separate steps.
 #
 
+import re
 import sys
+
+###############################################################################
+#
+# Define classes.
+#
+###############################################################################
 
 class Object(object):
   # Enable parentheses-free method calls, as in: retVal = myObj.method
@@ -29,19 +36,92 @@ class Object(object):
   def __getitem__(self, name): return self.__getattribute__(name)
   def __setitem__(self, name, value): self.__dict__[name] = value
 
+class Rule(Object):
+  pass
+
+class SeqRule(Rule):
+  def __init__(self, name, seq):
+    self.name = name
+    self.seq = seq
+  def parse(self, code, pos):
+    pieces = {}
+    startpos = pos
+    for ruleName in self.seq:
+      if ruleName[0] == "'": val, pos = parse_exact_str(ruleName[1:], code, pos)
+      elif ruleName[0] == '"': val, pos = parse_exact_re(ruleName[1:-1], code, pos)
+      else:
+        val, pos = rules[ruleName].parse(code, pos)
+        if val: pieces[ruleName] = pieces.get(ruleName, []) + [val[ruleName]]
+      if val is None: return None, startpos
+    for key in pieces:
+      if len(pieces[key]) == 1: pieces[key] = pieces[key][0]
+    result = {self.name: (pieces if len(pieces) else val)}
+    return result, pos
+
+class OrRule(Rule):
+  def __init__(self, name, or_list):
+    self.name = name
+    self.or_list = or_list
+  def parse(self, code, pos):
+    for r in self.or_list:
+      val, pos = rules[r].parse(code, pos)
+      if val: return {self.name: val}, pos
+    return None, pos
+
+###############################################################################
+#
+# Define functions.
+#
+###############################################################################
+
+def parse_exact_str(s, code, pos):
+  to_escape = list("+()")
+  for e in to_escape: s = s.replace(e, "\\" + e)
+  return parse_exact_re(s, code, pos)
+
+# TODO Consider ways to do this witout using globals.
+
+errPos = -1
+errExpected = None
+
+def parse_exact_re(s, code, pos):
+  m = re.match(r"(%s)\s*" % s, code[pos:])
+  if m: return m.group(1), pos + len(m.group(0))
+  global errPos, errExpected
+  if pos > errPos: errExpected, errPos = s, pos
+  return None, pos
+
+
+###############################################################################
+#
+# Medium-level code.
+#
+# These are any functions called directly by any high-level code.
+#
+###############################################################################
+
+def get_base_rules():
+  rules = Object()
+  rules.phrase = SeqRule('phrase', ["'hi", "'there"])
+  return rules
+
 ###############################################################################
 #
 # Begin high-level goal code.
 #
 ###############################################################################
 
-def read_lang_def(filename):
-  pass
-
 def exec_file(filename):
-  pass
+  f = open(filename)
+  code = f.read()
+  f.close()
+  rules = get_base_rules()
+  tree, pos = rules.phrase.parse(code, 0)
+  while tree:
+    print(tree)
+    tree, pos = rules.phrase.parse(code, pos)
+
 
 if __name__ == '__main__':
-  read_lang_def('langauge definition2.water')
-  exec_file('sample1.water')
+  exec_file('language definition2.water')
 
