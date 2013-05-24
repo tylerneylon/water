b0d15eb0fe9750cb6866a90c03efd120fcfde7b8
Tyler Neylon <tylerneylon@gmail.com>
2013-05-24 08:05:59 -0700

    Major cleanup in aisle showwork.py.
    Next I plan to merge showwork-html into this, or perhaps make it another module,
    and then move away from showwork stuff for a little bit.

--- modules/showwork/showwork.py
+++ modules/showwork/showwork.py
@@ -14,8 +14,12 @@ import parse
 import run
 import sys
 
-# TODO All of the code from here until the main clause at the bottom is for
-#      showwork. It belongs in a separate module, not here.
+# NOTE: I decided that functions in modules don't need to be named as if
+#       they're private like _this since they aren't necessarily treated as
+#       libraries by other user. Of course, individual module writers can do
+#       what they like, and there may be modules that do act like libraries,
+#       in which case private names could make sense.
+
 
 # Expects two pairs; returns -1, 0, or 1 when r1 < r2,
 # r1 ~ r2, or r1 > r2.
@@ -69,14 +73,28 @@ def merge_same_ranges(ranges, code_strs):
       out_strs.append(code_strs[i])
   return out_ranges, out_strs
 
-# This function is here since it doesn't belong directly in the core. The core
-# is only responsible for making this function possible.
-def showwork(filename):
-  run.run_code = False
-  parse.runfile(filename)
+# Returns a list of word-wrapped lines from one newline-free line.
+def word_wrap(one_line, w):
+  if one_line == '': return ['']
+  short_lines = []
+  while len(one_line) > w:
+    short_lines.append(one_line[:w])
+    one_line = one_line[w:]
+  if one_line: short_lines.append(one_line)
+  return short_lines
 
-  # 1. Load in the src and find a list of char ranges for each line.
+# Separates one newline-including string into lines word-wrapped at w chars.
+def make_lines(long_string, w):
+  if long_string is None: long_string = ''
+  lines = long_string.split('\n')
+  i = 0
+  while i < len(lines):
+    short_lines = word_wrap(lines[i], w)
+    lines[i:i + 1] = short_lines
+    i += len(short_lines)
+  return lines
 
+def get_src_ranges_and_lines(filename):
   f = open(filename)
   src = f.read()
   # TODO I just realized I'm not using the terms src & code consistently.
@@ -93,29 +111,28 @@ def showwork(filename):
     size = len(line) + 1
     src_ranges.append((idx, idx + size))
     idx += size
+  return src_ranges, src_lines
 
-
-  # 2. Consolidate code info so consecutive ranges are distinct.
-
+# Splits/rejoins code so lines are together and consecutive ranges are distinct.
+def get_code_ranges_and_lines():
   code_ranges = map(range_of_state, run.state_list)
   code_ranges, code_list = drop_middle_newlines(code_ranges, run.code_list)
   code_ranges, code_list = merge_into_lines(code_ranges, code_list)
-  code_ranges, code_list = merge_same_ranges(code_ranges, code_list)
+  return merge_same_ranges(code_ranges, code_list)
 
-
-  # 3. Build a list of concurrent (src, code) string pairs.
-
+# Builds a list of concurrent (src, code) string pairs, called chunks.
+def chunks_from_src_code_info(s_ranges, s_lines, c_ranges, c_list):
   # None acts as a stopping mark for both range lists.
-  src_ranges.append(None)
-  code_ranges.append(None)
+  s_ranges.append(None)
+  c_ranges.append(None)
 
   chunks = []
   last_chunk = (0, 0)  # This will be < whatever is next.
-  next_code = code_ranges[0] if len(code_ranges) else None
-  next_src = src_ranges[0] if len(src_ranges) else None
+  next_code = c_ranges[0] if len(c_ranges) else None
+  next_src = s_ranges[0] if len(s_ranges) else None
   s, c = 0, 0  # s & c are the src & code indices.
-  sline = src_lines[s] if next_src else ''
-  cline = code_list[c] if next_code else ''
+  sline = s_lines[s] if next_src else ''
+  cline = c_list[c] if next_code else ''
   # I'm leaving in some commented-out print statements because I'm not 100% that
   # the current chunking heuristics are optimal. If I want to experiment, I can
   # uncomment the prints to help see the decisions being made.
@@ -128,7 +145,8 @@ def showwork(filename):
     if code: chunks[-1][1] += code
   while next_code and next_src:
     #print('\nlast_chunk is %s' % (last_chunk,))
-    #print('code@%s is %s\nsrc@%s is %s\n' % (next_code, `cline`, next_src, `sline`))
+    #print('code@%s is %s\nsrc@%s is %s\n' %
+    #      (next_code, `cline`, next_src, `sline`))
     # We have both new src and new code to add.
     if rng_cmp(next_src, next_code) == 0:
       # Next src & code overlap, so make them a new chunk. The lists were
@@ -158,16 +176,17 @@ def showwork(filename):
           new_chunk(None, cline)
           c += 1
           last_chunk = next_code
-    next_src = src_ranges[s]
-    next_code = code_ranges[c]
-    sline = src_lines[s] if next_src else ''
-    cline = code_list[c] if next_code else ''
+    next_src = s_ranges[s]
+    next_code = c_ranges[c]
+    sline = s_lines[s] if next_src else ''
+    cline = c_list[c] if next_code else ''
   if next_code or next_src: chunks.append(['', ''])  # Chunk to hold the rest.
-  while s < len(src_lines): add_to_last(src_lines[s], None); s += 1
-  while c < len(code_list): add_to_last(None, code_list[c]); c += 1
+  while s < len(s_lines): add_to_last(s_lines[s], None); s += 1
+  while c < len(c_list): add_to_last(None, c_list[c]); c += 1
+  return chunks
 
-  # 3.5. Combine consecutive chunks with the same side empty.
-
+# Combines consecutive chunks with the same side empty.
+def combine_consec_empty_sides(chunks):
   def do_combine(prev_chunks, next_chunk):
     if len(prev_chunks) == 0: return False
     for i in [0, 1]:
@@ -182,10 +201,11 @@ def showwork(filename):
           new_chunks[-1][i] += ('\n' + chunk[i])
     else:
       new_chunks.append(chunk)
-  chunks = new_chunks
+  return new_chunks
 
-  # 4. Collate, word-wrap, and print the lines side-by-side.
-
+# Returns good widths, cols for printing chunks to the current console, where
+# widths = [left_w, right_w] and cols = sum(widths) + 1 for the middle | column.
+def find_good_col_widths(chunks):
   cols = int(os.popen('stty size').read().split()[1])
   max_cols = 200
   if cols > max_cols: cols = max_cols
@@ -203,29 +223,10 @@ def showwork(filename):
     widths[i] = max_w[i]
     # Make other width as small as the text it contains, if possible.
     widths[1 - i] = min(max_w[1 - i], cols - 1 - widths[i])
+  return widths
+
+def print_chunks_in_columns(chunks, widths):
   cols = widths[0] + 1 + widths[1]
-
-  # Returns a list of word-wrapped lines from one newline-free line.
-  def word_wrap(one_line, w):
-    if one_line == '': return ['']
-    short_lines = []
-    while len(one_line) > w:
-      short_lines.append(one_line[:w])
-      one_line = one_line[w:]
-    if one_line: short_lines.append(one_line)
-    return short_lines
-
-  # Separates one newline-including string into word-wrapped lines.
-  def make_lines(long_string, w):
-    if long_string is None: long_string = ''
-    lines = long_string.split('\n')
-    i = 0
-    while i < len(lines):
-      short_lines = word_wrap(lines[i], w)
-      lines[i:i + 1] = short_lines
-      i += len(short_lines)
-    return lines
-
   for chunk in chunks:
     print('-' * cols)
     left_lines = make_lines(chunk[0], widths[0])
@@ -236,6 +237,29 @@ def showwork(filename):
       print('%%-%ds|%%-%ds' % (widths[0], widths[1]) % (left, right))
     map(print_a_line, left_lines, right_lines)
   print('-' * cols)
+
+# This function is here since it doesn't belong directly in the core. The core
+# is only responsible for making this function possible.
+def showwork(filename):
+  run.run_code = False
+  parse.runfile(filename)
+
+  # 1. Get convenient sequences of ranges and line-based strings.
+  src_ranges, src_lines = get_src_ranges_and_lines(filename)
+  # We call code_list a "list" and not "lines" as some strings may be multiple
+  # lines, which is what we want so code ranges are always changing.
+  code_ranges, code_list = get_code_ranges_and_lines()
+
+  # 2. Build concurrent, maybe uneven, line-based src/code string pairs.
+  # This is the most nontrivial part of this entire function.
+  chunks = chunks_from_src_code_info(src_ranges, src_lines,
+                                     code_ranges, code_list)
+
+  # 3. Preserve src/code mapping but collate & format for clear printing.
+  chunks = combine_consec_empty_sides(chunks)
+  widths = find_good_col_widths(chunks)
+  print_chunks_in_columns(chunks, widths)
+
 
 # We expect args[0] to be the part of the shell command up to and including our
 # command name - which may contain spaces, and have been considered different
