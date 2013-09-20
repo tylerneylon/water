3cb384b3d5dafb53b17e67a7ade4822a76f589e7
Tyler Neylon <tylerneylon@gmail.com>
2013-09-20 09:05:28 -0700

    Drop some obsolete comments.

--- bin/make_base_grammar_cmd.sh
+++ bin/make_base_grammar_cmd.sh
@@ -6,9 +6,6 @@
 # which is used by parse.py to know the common base grammar used
 # to parse all other water files.
 #
-# This script uses the commands module along with the files
-# bootstrap.water and base_grammar.water.
-#
 
 # Use a temporary file since a redirected output destination cannot
 # be read from; e.g. "cat file > file" will result in file being empty,
